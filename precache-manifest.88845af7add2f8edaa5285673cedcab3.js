540e88c8a8d22361433e3f17e13cc511c28ae2c1
xiaomo <tangjijun@1ziton.com>
2019-07-01 15:04:09 +0800

    Updates

---
+++ precache-manifest.88845af7add2f8edaa5285673cedcab3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11038d9cad7986b866a21951f9f9e9b5",
+    "url": "/zsf-app/index.html"
+  },
+  {
+    "revision": "3250a9e98b2ea7b4bac8",
+    "url": "/zsf-app/static/css/main.b03546c8.chunk.css"
+  },
+  {
+    "revision": "6c7dbc42cb4d4027caf4",
+    "url": "/zsf-app/static/js/2.ff285e66.chunk.js"
+  },
+  {
+    "revision": "3250a9e98b2ea7b4bac8",
+    "url": "/zsf-app/static/js/main.7d4d352b.chunk.js"
+  },
+  {
+    "revision": "20fcc5224ffc93fdc017",
+    "url": "/zsf-app/static/js/runtime~main.5f7439a6.js"
+  }
+]);
